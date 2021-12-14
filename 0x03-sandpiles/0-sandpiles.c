25df3dd457c3993f16dd87b063a792cb8247e298
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-12-14 12:04:35 -0600

    Updating betty style

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -24,12 +24,11 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		print_grid(grid1);
 
 		for (i = 0; i < 3; i++)
-		{
-		   for (j = 0; j < 3; j++)
-		   {
-			   temp[i][j] = 0;
-		   }
-		}
+			for (j = 0; j < 3; j++)
+			{
+				temp[i][j] = 0;
+	   		}
+
 		for (i = 0; i < 3; i++)
 		{
 			for (j = 0; j < 3; j++)
