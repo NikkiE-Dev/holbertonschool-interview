cf42920a48c483e7c77d9f018b8fc50756608bc9
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-12-22 18:38:00 -0600

    Adding linked list function

---
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome- checks if a linked list is a palindrome
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL)
+		return (1);
+	else
+		return (palindrome_check(head, *head));
+}
+
+/**
+ * palindrome_check- checks if a linked list is a palindrome
+ * @initial: first linked list
+ * @reverse: reversed linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int palindrome_check(listint_t **initial, listint_t *reverse)
+	{
+		if (reverse == NULL)
+			return (1);
+		if (reverse->n != (*initial)->n)
+			return (0);
+		*initial = (*initial)->next;
+		return (1);
+	}
