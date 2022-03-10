979c85846565d82d7a577f9b81929cbd68ad572e
Nikki-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2022-03-10 11:17:03 -0600

    Adding code

---
+++ 0x00-rain/0-rain.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+rain
+"""
+
+
+def rain(walls):
+    i = 0
+    drops = 0
+    collected = 0
+    frt_col = 0
+    back_col = 0
+
+    if len(walls) == 0:
+        return 0
+    for i in range(len(walls)):
+        if walls[i] == 0:
+            if i == (len(walls) - 1):
+                return collected
+            if frt_col > 0:
+                drops += 1
+        elif walls[i] > 0:
+            if frt_col > 0:
+                back_col = walls[i]
+                if back_col >= frt_col:
+                    collected += (drops * frt_col)
+                else:
+                    collected += (drops * back_col)
+                drops = 0
+                frt_col = back_col
+                back_col = 0
+            else:
+                frt_col = walls[i]
+
+    return collected
