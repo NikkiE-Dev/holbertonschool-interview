9e8e85b784a321825ccd3417c5c76d8aaa283ddf
Nikki-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2022-03-10 13:46:28 -0600

    Adding conditions

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -7,31 +7,14 @@ rain
 def rain(walls):
     i = 0
     drops = 0
-    collected = 0
-    frt_col = 0
-    back_col = 0
 
-    if type(walls) is not list:
+    if not walls:
         return 0
     if len(walls) == 0:
         return 0
-    for i in range(len(walls)):
-        if walls[i] == 0:
-            if i == (len(walls) - 1):
-                return collected
-            if frt_col > 0:
-                drops += 1
-        elif walls[i] > 0:
-            if frt_col > 0:
-                back_col = walls[i]
-                if back_col >= frt_col:
-                    collected += (drops * frt_col)
-                else:
-                    collected += (drops * back_col)
-                drops = 0
-                frt_col = back_col
-                back_col = 0
-            else:
-                frt_col = walls[i]
-
-    return collected
+    while i <= (len(walls) - 1):
+        left_wall = max(walls[:i + 1])
+        right_wall = max(walls[i:])
+        drops += max(min(left_wall, right_wall) - walls[i], 0)
+        i += 1
+    return drops
