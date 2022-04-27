075b9df7b086d5419d14acd3a089bb5792bccb57
Nikki-Holberton <78047506+NikkiE-Dev@users.noreply.github.com>
2022-04-27 17:13:53 -0500

    Updating code

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -4,28 +4,29 @@ Determine the fewest number of coins needed to meet a given amount total
 """
 
 
-def makeChange(coins, amount):
+def makeChange(coins, total):
     """
     Making change
     """
-    temp = []
-    total = []
-    if amount <= 0:
+    num_of_coins = []
+    coin_list = []
+    if total <= 0:
         return 0
     else:
-        while amount != 0:
+        while total != 0:
             if len(coins) == 0:
                 return -1
-            if amount % max(coins) == 0:
-                temp.append((max(coins), amount//max(coins)))
-                amount = 0
-            elif max(coins) < amount:
-                temp.append((max(coins), amount//max(coins)))
-                amount = amount % max(coins)
+            if total % max(coins) == 0:
+                num_of_coins.append((max(coins), total//max(coins)))
+                total = 0
+            elif max(coins) < total:
+                num_of_coins.append((max(coins), total//max(coins)))
+                total = total % max(coins)
                 coins.remove(max(coins))
             else:
                 coins.remove(max(coins))
-    for item in temp:
-        total.append(item[1])
-    temp = sum(total)
-    return temp
+    for item in num_of_coins:
+        coin_list.append(item[1])
+    num_of_coins = sum(coin_list)
+    return num_of_coins
+
