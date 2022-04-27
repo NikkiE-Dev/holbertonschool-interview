09572982d7deacc74fb2aafa610c14ad6005dd8b
Nikki-Holberton <78047506+NikkiE-Dev@users.noreply.github.com>
2022-04-27 13:03:05 -0500

    Updating code & making PEP8 compliant

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Making change
+Determine the fewest number of coins needed to meet a given amount total
 """
 
 
@@ -8,4 +8,24 @@ def makeChange(coins, amount):
     """
     Making change
     """
-    
+    temp = []
+    total = []
+    if amount <= 0:
+        return 0
+    else:
+        while amount != 0:
+            if len(coins) == 0:
+                return -1
+            if amount % max(coins) == 0:
+                temp.append((max(coins), amount//max(coins)))
+                amount = 0
+            elif max(coins) < amount:
+                temp.append((max(coins), amount//max(coins)))
+                amount = amount % max(coins)
+                coins.remove(max(coins))
+            else:
+                coins.remove(max(coins))
+    for item in temp:
+        total.append(item[1])
+    temp = sum(total)
+    return temp
